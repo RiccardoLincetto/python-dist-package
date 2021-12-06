5d02147b330de04b3dc483a3007e2afa897d7b95
Riccardo Lincetto <riccardo.lincetto@gmail.com>
2021-12-06 03:00:27 +0100

    Pylint tests fixes

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,9 +1,13 @@
+"""Run tests on pypack.module"""
+
 import unittest
 
 from src.pypack import module
 
 
 class TestHelloFunc(unittest.TestCase):
+    """Test pypack.module.get_hello"""
 
     def test_hello(self):
+        """Check output."""
         self.assertEqual(module.get_hello(), "Hello!")
